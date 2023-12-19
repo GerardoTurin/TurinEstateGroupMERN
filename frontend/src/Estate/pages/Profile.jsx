c45191a0c419e3b282f74f98292ef97833a6a6f0
Gerardo <gera_1993_06@hotmail.com>
2023-12-19 16:09:29 -0300

    Add user profile form and functionality

--- frontend/src/Estate/pages/Profile.jsx
+++ frontend/src/Estate/pages/Profile.jsx
@@ -1,7 +1,59 @@
+import useAuthStore from "../../auth/hooks/useAuthStore";
+import useForm from "../../auth/hooks/useForm";
 
 const Profile = () => {
+    
+    const { user } = useAuthStore();
+    const { name, email, photo } = user;
+    const { onInputChange } = useForm( user );
+    
     return (
-        <div>Profile</div>
+        <div className="p-3 max-w-lg mx-auto">
+            <h1 className="text-3xl text-center my-7 font-semibold">
+                Profile
+            </h1>
+
+            <form className="flex flex-col mx-auto gap-3">
+                <img
+                    className="w-24 h-24 rounded-full object-cover cursor-pointer mx-auto"
+                    src={photo}
+                    alt="user profile"
+                />
+                <input
+                    type="text"
+                    className="border border-gray-300 rounded-md p-3"
+                    placeholder="Name"
+                    value={name} 
+                    id="name"
+                    autoComplete="off"
+                    onChange={ onInputChange }
+                    />
+                <input
+                    type="email"
+                    className="border border-gray-300 rounded-md p-3"
+                    placeholder="Email"
+                    value={email} 
+                    id="email"
+                    autoComplete="off"
+                    onChange={ onInputChange }
+                    />
+
+                <button
+                    type="submit"
+                    className="bg-blue-500 text-white rounded-md p-3"
+                    >
+                    Update
+                </button>
+            </form>
+            <div className="flex mt-3 justify-between">
+                <span className="text-red-500 cursor-pointer font-semibold">
+                    Delete Account
+                </span>
+                <span className="text-blue-500 cursor-pointer font-semibold">
+                    Sign Out
+                </span>
+            </div>
+        </div>
     )
 };
 
